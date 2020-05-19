237ca9674a26969f89274658e242288d3e8149bd
N-M <nagaomasaaki@masaaki.local>
2020-05-19 19:51:35 +0900

    first commit

---
+++ periodic_table.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+#A = 原子番号または文字列
+def element_dict(A): 
+    PERIODIC_TABLE=[
+    "H","He",
+    "Li","Be","B","C","N","O","F","Ne",
+    "Na","Mg","Al","Si","P","S","Cl","Ar",
+    "K","Ca","Sc","Ti","V","Cr","Mn","Fe","Co","Ni","Cu","Zn","Ga","Ge","As","Se","Br","Kr",
+    "Rb","Sr","Y","Zr","Nb","Mo","Tc","Ru","Rh","Pd","Ag","Cd","In","Sn","Sb","Te","I","Xe",
+    "Cs","Ba",
+    "La","Ce","Pr","Nd","Pm","Sm","Eu","Gd","Tb","Dy","Ho","Er","Tm","Yb","Lu",
+    "Hf","Ta","W","Re","Os","Ir","Pt","Au","Hg","Tl","Pb","Bi","Po","At","Rn",
+    "Fr","Ra",
+    "Ac","Th","Pa","U","Np","Pu","Am","Cm","Bk","Cf","Es","Fm","Md","No","Lr",
+    "Rf","Db","Sg","Bh","Hs","Mt","Ds","Rg","Cn","Nh","Fl","Mc","Lv","Ts","Og"]
+    
+    if type(A) == int:
+        aim = PERIODIC_TABLE[A-1]
+    elif type(A) == str:
+        aim = PERIODIC_TABLE.index(A) + 1
+        
+    return aim
+
+
+
